acabdd22185af023f30732fbb0e44f54d163f50a
grgrgrrr <grgrgrrr29@gmail.com>
2022-08-11 21:41:29 +0200

    25% course progress

---
+++ cards/deck.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five"}
+
+	for _, cs := range cardSuits {
+		for _, cv := range cardValues {
+			cards = append(cards, cs+" of "+cv)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
